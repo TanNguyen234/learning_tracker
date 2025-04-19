22f3dc6eeec0b93d366b5a0eac8ecc8099b15ed4
Tan Nguyen <tanntd.2005@gmail.com>
2025-04-19 19:31:47 +0700

    addSkill page

--- src/pages/skill/addSkill.jsx
+++ src/pages/skill/addSkill.jsx
@@ -1,13 +1,15 @@
-import { Button, Form, Input, message, Space } from 'antd';
+import './style.scss'
+import { Button, Form, Input, message, Select, Space } from 'antd';
 
 function AddSkill() {
     const [form] = Form.useForm();
-    const onFinish = () => {
-        message.success('Submit success!');
-        console.log(form)
+    const onFinish = (values) => {
+        message.success('Đã thêm 1 kỹ năng mới!');
+        form.resetFields()
+        console.log(values)
     };
     const onFinishFailed = () => {
-        message.error('Submit failed!');
+        message.error('Thêm kỹ năng mới thất bại!');
     };
     return (
         <Form
@@ -16,25 +18,40 @@ function AddSkill() {
           onFinish={onFinish}
           onFinishFailed={onFinishFailed}
           autoComplete="off"
+          initialValues={{ state: 'Learning' }}
         >
+          <h1 style={{textAlign: 'center'}}>Thêm kỹ năng</h1>
           <Form.Item
             name="title"
-            label="Tiêu đề"
+            label="Tiêu đề:"
             rules={[{ required: true }, { type: 'text', warningOnly: true }, { type: 'string', min: 1 }]}
           >
-            <Input type='text' placeholder="Nhâp tên kĩ năng" />
+            <Input type='text' placeholder="Nhập tên kĩ năng" />
+          </Form.Item>
+          <Form.Item
+            label="Trạng thái:"
+            name="state"
+            rules={[{ required: true, message: 'Vui lòng thêm trạng thái!' }]}
+          >
+            <Select 
+              options={[
+                { value: 'Planed', label: 'Planed' },
+                { value: 'Learning', label: 'Learning' },
+                { value: 'Done', label: 'Done' },
+              ]}
+            >
+            </Select>
           </Form.Item>
           <Form.Item
             name="description"
-            label="Mô tả"
-            rules={[{ required: true }, { type: 'text', warningOnly: true }, { type: 'string', min: 1 }]}
+            label="Mô tả:"
           >
             <Input.TextArea rows={6} />
           </Form.Item>
-          <Form.Item>
+          <Form.Item style={{textAlign: 'center'}}>
             <Space>
-              <Button type="primary" htmlType="submit">
-                Submit
+              <Button size="large" type="primary" htmlType="submit">
+                Thêm
               </Button>
             </Space>
           </Form.Item>
